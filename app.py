f9640e6fde657522e1590b255abfe6aacd95e056
manoj-nathwani <manoj_1993@hotmail.com>
2015-05-03 14:31:46 +0100

    added investors + mentors

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 class MainPage(webapp2.RequestHandler):
     def get(self):
         startups = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
-        investors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
-        mentors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
+        investors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/data/investors.json").content)
+        mentors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/data/mentors.json").content)
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render({"startups": startups, "investors": investors, "mentors": mentors}))
 
