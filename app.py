b9f71c7dbcb0a36e49d1ba8272be80f6a4142a1f
manoj-nathwani <manoj_1993@hotmail.com>
2015-05-03 14:31:04 +0100

    added investors + mentors

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import jinja2
 import webapp2
 from google.appengine.api import urlfetch
 import json
-import Queue
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -12,20 +11,12 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class MainPage(webapp2.RequestHandler):
-
     def get(self):
-        response = urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json")
-        startups = json.loads(response.content)
-        q = Queue.Queue()
-        q.put("2ecc71", "3498db")
-        while True:
-            if len(startups) % 3 == 0:
-                break
-            else:
-                startups.append({"image_650x350": "http://placehold.it/650x350/" + q.get() + "/000000&text=Coming soon..."})
-
+        startups = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
+        investors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
+        mentors = json.loads(urlfetch.fetch("https://raw.githubusercontent.com/BanterVentures/Portfolio/master/startups.json").content)
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render({"startups": startups}))
+        self.response.write(template.render({"startups": startups, "investors": investors, "mentors": mentors}))
 
 
 application = webapp2.WSGIApplication([
